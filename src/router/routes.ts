9c5a97e1356c1ce5a823a3c0a4ccb38dd28a8cb2
Mohsen Pakzad <mohsen137853@gmail.com>
2022-09-02 02:02:25 +0430

    Setup router and add process list to it

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -4,7 +4,10 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    children: [
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: '/process-list', component: () => import('pages/ProcessList.vue') },
+    ],
   },
 
   // Always leave this as last one,
