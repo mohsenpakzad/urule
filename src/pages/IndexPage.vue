286d9fde5fff0f552f01d4ac741bc99acda837f8
Mohsen Pakzad <mohsen137853@gmail.com>
2022-10-01 08:48:35 +0330

    Move index page critical states to main store due data lost when navigating between pages and polish the code

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
-import { computed, reactive, ref } from 'vue';
+import { computed, ref } from 'vue';
+import { storeToRefs } from 'pinia';
 import { useStore } from 'stores/main';
-import { QForm, useQuasar } from 'quasar';
+import { useQuasar } from 'quasar';
 import { useUruleCore } from 'src/composables/useUruleCore';
 import { useFormatter } from 'src/composables/useFormatter';
 import { useRules } from 'src/composables/useRules';
 import { Address } from 'src/models/core';
-import { ScanState, ScanType, ValueType } from 'src/models/scan'
+import { ScanState } from 'src/models/scan';
 
 const store = useStore();
 const uruleCore = useUruleCore();
@@ -14,8 +15,23 @@ const formatter = useFormatter();
 const rules = useRules();
 const q = useQuasar();
 
+const {
+  valueTypes: valueTypeOptions,
 
-const scanState = ref<ScanState>(ScanState.BeforeInitialScan);
+  scanData,
+} = store;
+
+const {
+  openedProcess,
+
+  scanState,
+  scanForm,
+  scanTypeOptions,
+  scanTypeOptionsRequiredInputs,
+
+  addressList,
+  selectedAddresses,
+} = storeToRefs(store);
 
 const addressTableColumns = [
   {
@@ -35,158 +51,9 @@ const addressTableColumns = [
     field: (p: Address) => p.value,
   },
 ];
-const addressList = ref<Address[]>();
-const selectedAddresses = ref<Address[]>([]);
-
-const allScanTypeOptions = [
-  {
-    label: 'Exact value',
-    value: ScanType.Exact,
-    availability: ScanState.BeforeInitialScan | ScanState.AfterInitialScan,
-  },
-  {
-    label: 'Value between',
-    value: ScanType.InRange,
-    availability: ScanState.BeforeInitialScan | ScanState.AfterInitialScan,
-  },
-  {
-    label: 'Unknown initial value',
-    value: ScanType.Unknown,
-    availability: ScanState.BeforeInitialScan,
-  },
-  {
-    label: 'Increased value',
-    value: ScanType.Increased,
-    availability: ScanState.AfterInitialScan,
-  },
-  {
-    label: 'Increased value by',
-    value: ScanType.IncreasedBy,
-    availability: ScanState.AfterInitialScan,
-  },
-  {
-    label: 'Decreased value',
-    value: ScanType.Decreased,
-    availability: ScanState.AfterInitialScan,
-  },
-  {
-    label: 'Decreased value by',
-    value: ScanType.DecreasedBy,
-    availability: ScanState.AfterInitialScan,
-  },
-  {
-    label: 'Changed value',
-    value: ScanType.Changed,
-    availability: ScanState.AfterInitialScan,
-  },
-  {
-    label: 'Unchanged value',
-    value: ScanType.Unchanged,
-    availability: ScanState.AfterInitialScan,
-  },
-];
-const scanTypeOptions = computed(() => {
-  return allScanTypeOptions.filter((e) => e.availability & scanState.value);
-});
-
-const valueTypeOptions = [
-  {
-    label: 'Signed Byte',
-    value: ValueType.I8,
-    min: (-2) ** 7,
-    max: 2 ** 7 - 1,
-    format: rules.ruleInteger,
-  },
-  {
-    label: 'Unsigned Byte',
-    value: ValueType.U8,
-    min: 0,
-    max: 2 ** 8,
-    format: rules.ruleInteger,
-  },
-  {
-    label: 'Signed 2 Bytes',
-    min: (-2) ** 15,
-    max: 2 ** 15 - 1,
-    value: ValueType.I16,
-    format: rules.ruleInteger,
-  },
-  {
-    label: 'Unsigned 2 Bytes',
-    value: ValueType.U16,
-    min: 0,
-    max: 2 ** 16,
-    format: rules.ruleInteger,
-  },
-  {
-    label: 'Signed 4 Bytes',
-    value: ValueType.I32,
-    min: (-2) ** 31,
-    max: 2 ** 31 - 1,
-    format: rules.ruleInteger,
-  },
-  {
-    label: 'Unsigned 4 Bytes',
-    value: ValueType.U32,
-    min: 0,
-    max: 2 ** 32,
-    format: rules.ruleInteger,
-  },
-  {
-    label: 'Signed 8 Bytes',
-    value: ValueType.I64,
-    min: (-2) ** 63,
-    max: 2 ** 63 - 1,
-    format: rules.ruleInteger,
-  },
-  {
-    label: 'Unsigned 8 Bytes',
-    value: ValueType.U64,
-    min: 0,
-    max: 2 ** 64,
-    format: rules.ruleInteger,
-  },
-  {
-    label: 'Float 4 Bytes',
-    value: ValueType.F32,
-    min: -3.40282347e+38,
-    max: 3.40282347e+38,
-    format: rules.ruleDecimal,
-  },
-  {
-    label: 'Float 8 Bytes',
-    value: ValueType.F64,
-    min: -1.7976931348623157e+308,
-    max: 1.7976931348623157e+308,
-    format: rules.ruleDecimal,
-  },
-];
-
-const scanForm = ref<QForm>()
-const scanData = reactive({
-  scanType: allScanTypeOptions.find(e => e.value === ScanType.Exact),
-  valueType: valueTypeOptions.find(e => e.value === ValueType.I32),
-  value: {
-    exact: '',
-    range: {min: '', max: ''}
-  }
-})
 
 const changeValueDialog = ref<boolean>(false);
 const changeValueDialogInput = ref<string>('');
-
-const scanTypeOptionsRequiredInputs = computed(() => {
-  switch (scanData.scanType?.value) {
-    case ScanType.Exact:
-    case ScanType.DecreasedBy:
-    case ScanType.IncreasedBy:
-      return 1;
-    case ScanType.InRange:
-      return 2;
-    default:
-      return 0;
-  }
-});
 
 function baseScanValueRules() {
   return [
@@ -195,8 +62,8 @@ function baseScanValueRules() {
     rules.ruleBetween(scanData.valueType!.min, scanData.valueType!.max),
   ]
 }
-const scanValueRules = computed(() => baseScanValueRules())
 
+const scanValueRules = computed(() => baseScanValueRules())
 const scanValueMinRangeRules = computed(() => {
   const max = parseFloat(scanData.value.range.max);
   return [
@@ -250,7 +117,7 @@ function writeMemory() {
     const writtenBytes = await uruleCore.writeOpenedProcessMemory(address.pointer, value)
 
     // TODO: instead of this, fetch these addresses value from last scan again
-    if (writtenBytes){
+    if (writtenBytes) {
       address.value = value
     }
     console.log(`${writtenBytes} bytes written.`)
@@ -298,7 +165,7 @@ function writeMemory() {
                 @click.prevent="firstScan"
               >
                 <q-menu
-                  v-if="!store.openedProcess"
+                  v-if="!openedProcess"
                   anchor="top right"
                 >
                   <q-banner class="bg-accent text-white" dense>
@@ -442,7 +309,7 @@ function writeMemory() {
               </q-card-section>
 
               <q-card-section class="q-pt-none">
-              <!-- TODO: add more validation -->
+                <!-- TODO: add more validation -->
                 <q-input
                   dense
                   v-model="changeValueDialogInput"
--- src/stores/main.ts
+++ src/stores/main.ts
@@ -1,12 +1,184 @@
-import { ref } from 'vue';
+import { computed, reactive, ref } from 'vue';
 import { defineStore } from 'pinia';
-import { Process } from 'src/models/core';
+import { QForm } from 'quasar';
+import { useRules } from 'src/composables/useRules';
+import { Address, Process } from 'src/models/core';
+import { ScanState, ScanType, ValueType } from 'src/models/scan';
 
 export const useStore = defineStore('main', () => {
 
+  const rules = useRules();
+
+  const scanTypes = [
+    {
+      label: 'Exact value',
+      value: ScanType.Exact,
+      availability: ScanState.BeforeInitialScan | ScanState.AfterInitialScan,
+    },
+    {
+      label: 'Value between',
+      value: ScanType.InRange,
+      availability: ScanState.BeforeInitialScan | ScanState.AfterInitialScan,
+    },
+    {
+      label: 'Unknown initial value',
+      value: ScanType.Unknown,
+      availability: ScanState.BeforeInitialScan,
+    },
+    {
+      label: 'Increased value',
+      value: ScanType.Increased,
+      availability: ScanState.AfterInitialScan,
+    },
+    {
+      label: 'Increased value by',
+      value: ScanType.IncreasedBy,
+      availability: ScanState.AfterInitialScan,
+    },
+    {
+      label: 'Decreased value',
+      value: ScanType.Decreased,
+      availability: ScanState.AfterInitialScan,
+    },
+    {
+      label: 'Decreased value by',
+      value: ScanType.DecreasedBy,
+      availability: ScanState.AfterInitialScan,
+    },
+    {
+      label: 'Changed value',
+      value: ScanType.Changed,
+      availability: ScanState.AfterInitialScan,
+    },
+    {
+      label: 'Unchanged value',
+      value: ScanType.Unchanged,
+      availability: ScanState.AfterInitialScan,
+    },
+  ];
+  const valueTypes = [
+    {
+      label: 'Signed Byte',
+      value: ValueType.I8,
+      min: (-2) ** 7,
+      max: 2 ** 7 - 1,
+      format: rules.ruleInteger,
+    },
+    {
+      label: 'Unsigned Byte',
+      value: ValueType.U8,
+      min: 0,
+      max: 2 ** 8,
+      format: rules.ruleInteger,
+    },
+    {
+      label: 'Signed 2 Bytes',
+      min: (-2) ** 15,
+      max: 2 ** 15 - 1,
+      value: ValueType.I16,
+      format: rules.ruleInteger,
+    },
+    {
+      label: 'Unsigned 2 Bytes',
+      value: ValueType.U16,
+      min: 0,
+      max: 2 ** 16,
+      format: rules.ruleInteger,
+    },
+    {
+      label: 'Signed 4 Bytes',
+      value: ValueType.I32,
+      min: (-2) ** 31,
+      max: 2 ** 31 - 1,
+      format: rules.ruleInteger,
+    },
+    {
+      label: 'Unsigned 4 Bytes',
+      value: ValueType.U32,
+      min: 0,
+      max: 2 ** 32,
+      format: rules.ruleInteger,
+    },
+    {
+      label: 'Signed 8 Bytes',
+      value: ValueType.I64,
+      min: (-2) ** 63,
+      max: 2 ** 63 - 1,
+      format: rules.ruleInteger,
+    },
+    {
+      label: 'Unsigned 8 Bytes',
+      value: ValueType.U64,
+      min: 0,
+      max: 2 ** 64,
+      format: rules.ruleInteger,
+    },
+    {
+      label: 'Float 4 Bytes',
+      value: ValueType.F32,
+      min: -3.40282347e+38,
+      max: 3.40282347e+38,
+      format: rules.ruleDecimal,
+    },
+    {
+      label: 'Float 8 Bytes',
+      value: ValueType.F64,
+      min: -1.7976931348623157e+308,
+      max: 1.7976931348623157e+308,
+      format: rules.ruleDecimal,
+    },
+  ];
+
   const openedProcess = ref<Process>();
 
+  const scanState = ref<ScanState>(ScanState.BeforeInitialScan);
+  const scanData = reactive({
+    scanType: scanTypes.find(e => e.value === ScanType.Exact),
+    valueType: valueTypes.find(e => e.value === ValueType.I32),
+    value: {
+      exact: '',
+      range: {min: '', max: ''}
+    }
+  });
+  const scanForm = ref<QForm>();
+
+  const addressList = ref<Address[]>([]);
+  const selectedAddresses = ref<Address[]>([]);
+
+
+  const scanTypeOptions = computed(() => {
+    return scanTypes.filter((e) => e.availability & scanState.value);
+  });
+  const scanTypeOptionsRequiredInputs = computed(() => {
+    switch (scanData.scanType?.value) {
+      case ScanType.Exact:
+      case ScanType.DecreasedBy:
+      case ScanType.IncreasedBy:
+        return 1;
+      case ScanType.InRange:
+        return 2;
+      default:
+        return 0;
+    }
+  });
+
   return {
+    // constants
+    scanTypes,
+    valueTypes,
+
+    // refs and reactives
     openedProcess,
+
+    scanState,
+    scanData,
+    scanForm,
+
+    addressList,
+    selectedAddresses,
+
+    //  computed
+    scanTypeOptions,
+    scanTypeOptionsRequiredInputs,
   }
 });
