68e13ac4b98fc5a7257555247bce040a121399e8
Mohsen Pakzad <mohsen137853@gmail.com>
2022-10-01 04:52:56 +0330

    Add convertor function to convert regions to addresses and use it for better integrity of getLastScan function

--- src/composables/useUruleCore.ts
+++ src/composables/useUruleCore.ts
@@ -1,5 +1,5 @@
 import { invoke } from '@tauri-apps/api';
-import { Process, Region } from 'src/models/core';
+import { Address, Process, Region } from 'src/models/core';
 
 export function useUruleCore() {
 
@@ -12,7 +12,8 @@ export function useUruleCore() {
   }
 
   async function getLastScan() {
-    return await invoke<Region[]>('get_last_scan')
+    const lastScanRegions = await invoke<Region[]>('get_last_scan')
+    return convertRegionsToAddresses(lastScanRegions)
   }
 
   async function getProcesses() {
@@ -25,6 +26,42 @@ export function useUruleCore() {
 
   async function nextScan(scanStr: string) {
     await invoke<void>('next_scan', {scanStr})
+  }
+
+  function convertRegionsToAddresses(lastScanRegions: Region[]) {
+    return lastScanRegions.flatMap(region => {
+
+      if (region.locations.Discrete) {
+        const locations = region.locations.Discrete!.locations;
+        const value = byteArrayToLong(region.value.Exact!)
+
+        return locations.map(location => <Address>{pointer: location, value})
+      }
+      // TODO: handle other cases
+      return []
+    })
+  }
+
+  function longToByteArray(/*long*/long: number) {
+    // we want to represent the input as a 8-bytes array
+    const byteArray = [0, 0, 0, 0, 0, 0, 0, 0];
+
+    for (let index = 0; index < byteArray.length; index++) {
+      const byte = long & 0xff;
+      byteArray [index] = byte;
+      long = (long - byte) / 256;
+    }
+
+    return byteArray;
+  }
+
+  function byteArrayToLong(/*byte[]*/byteArray: number[]) {
+    let value = 0;
+    for (let i = byteArray.length - 1; i >= 0; i--) {
+      value = (value * 256) + byteArray[i];
+    }
+
+    return value;
   }
 
   return {
