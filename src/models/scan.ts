59fa75945a9ef17e7ff1eb96011b09598d6726f2
Mohsen Pakzad <mohsen137853@gmail.com>
2022-10-10 12:59:57 +0330

    Add new scan descriptor models

--- src/models/scan.ts
+++ src/models/scan.ts
@@ -1,13 +1,23 @@
+export interface ScanInfo {
+  typ: ScanType;
+  value: ScanValue;
+}
+
 export enum ScanType {
-  Exact,
-  Unknown,
-  InRange,
-  Unchanged,
-  Changed,
-  Decreased,
-  Increased,
-  DecreasedBy,
-  IncreasedBy,
+  Exact = 'Exact',
+  Unknown = 'Unknown',
+  InRange = 'InRange',
+  Unchanged = 'Unchanged',
+  Changed = 'Changed',
+  Decreased = 'Decreased',
+  Increased = 'Increased',
+  DecreasedBy = 'DecreasedBy',
+  IncreasedBy = 'IncreasedBy',
+}
+
+export interface ScanValue {
+  Exact?: string;
+  Range?: { start: string; end: string };
 }
 
 export enum ScanState {
@@ -16,14 +26,14 @@ export enum ScanState {
 }
 
 export enum ValueType {
-  I8,
-  U8,
-  I16,
-  U16,
-  I32,
-  U32,
-  I64,
-  U64,
-  F32,
-  F64,
+  I8 = 'I8',
+  U8 = 'U8',
+  I16 = 'I16',
+  U16 = 'U16',
+  I32 = 'I32',
+  U32 = 'U32',
+  I64 = 'I64',
+  U64 = 'U64',
+  F32 = 'F32',
+  F64 = 'F64',
 }
