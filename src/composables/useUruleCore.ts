ee3977c1e63f9bbd365744e760541c94e43d4a68
Mohsen Pakzad <mohsen137853@gmail.com>
2022-10-10 13:01:15 +0330

    Remove unused codes

--- src/composables/useUruleCore.ts
+++ src/composables/useUruleCore.ts
@@ -53,30 +53,6 @@ export function useUruleCore() {
     });
   }
 
-  // TODO: clean this code
-  function longToByteArray(/*long*/ long: number) {
-    // we want to represent the input as a 8-bytes array
-    const byteArray = [0, 0, 0, 0, 0, 0, 0, 0];
-
-    for (let index = 0; index < byteArray.length; index++) {
-      const byte = long & 0xff;
-      byteArray[index] = byte;
-      long = (long - byte) / 256;
-    }
-
-    return byteArray;
-  }
-
-  // TODO: clean this code
-  function byteArrayToLong(/*byte[]*/ byteArray: number[]) {
-    let value = 0;
-    for (let i = byteArray.length - 1; i >= 0; i--) {
-      value = value * 256 + byteArray[i];
-    }
-
-    return value;
-  }
-
   return {
     getOpenedProcess,
     writeOpenedProcessMemory,
