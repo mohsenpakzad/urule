030a70e75b3b6760e7bc92fe979f556f92078547
Mohsen Pakzad <mohsen137853@gmail.com>
2022-10-10 13:10:11 +0330

    Expand impl_scan generality to implement all scannable types

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -15,41 +15,6 @@ use scan::scan_meta::{ScanInfo, ValueType};
 use std::sync::Mutex;
 use winapi::um::winnt;
 
-pub struct AppState {
-    opened_process: Mutex<Option<Process>>,
-    scan_value_type: Mutex<ValueType>,
-    last_scan: Mutex<Vec<Region<4, i32>>>,
-}
-
-impl AppState {
-    fn new() -> Self {
-        AppState {
-            opened_process: Mutex::new(None),
-            scan_value_type: Mutex::new(ValueType::I32),
-            last_scan: Mutex::new(Vec::new()),
-        }
-    }
-
-    fn clear(&mut self) {
-        self.last_scan.lock().unwrap().clear();
-    }
-}
-
-fn main() {
-    tauri::Builder::default()
-        .manage(AppState::new())
-        .invoke_handler(tauri::generate_handler![
-            get_processes,
-            get_opened_process,
-            write_opened_process_memory,
-            get_last_scan,
-            first_scan,
-            next_scan,
-        ])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
-}
-
 #[tauri::command]
 fn get_processes() -> Vec<ProcessView> {
     process::enum_proc()
@@ -80,76 +45,115 @@ fn get_opened_process(state: tauri::State<AppState>) -> Option<ProcessView> {
 
 macro_rules! impl_scan {
     ( $( $type:ty : $type_size:expr ),+ ) => {
-        $(paste! {
-            #[tauri::command]
-            fn write_opened_process_memory(
-                address: usize,
-                value: $type,
-                state: tauri::State<AppState>,
-            ) -> Option<usize> {
-                state
-                    .opened_process
-                    .lock()
-                    .unwrap()
-                    .as_ref()
-                    .unwrap()
-                    .write_memory(address, &value)
-                    .ok()
+        paste! {
+            pub struct AppState {
+                opened_process: Mutex<Option<Process>>,
+                scan_value_type: Mutex<ValueType>,
+                $([<last_scan_ $type>]: Mutex<Vec<Region<$type_size, $type>>>,)+
             }
 
-            #[tauri::command]
-            fn get_last_scan(state: tauri::State<AppState>) -> Vec<Region<$type_size, $type>> {
-                state.last_scan.lock().unwrap().clone()
+            impl AppState {
+                fn new() -> Self {
+                    AppState {
+                        opened_process: Mutex::new(None),
+                        scan_value_type: Mutex::new(ValueType::I32),
+                        $([<last_scan_ $type>]: Mutex::new(Vec::new()),)+
+                    }
+                }
+
+                fn clear(&mut self) {
+                    $(self.[<last_scan_ $type>].lock().unwrap().clear();)+
+                }
             }
 
-            #[tauri::command]
-            fn first_scan(pid: u32, value_type: ValueType, scan_info: ScanInfo, state: tauri::State<AppState>) {
-                let process = Process::open(pid).unwrap();
-                println!("Opened process {:?}", process);
-
-                const MASK: u32 = winnt::PAGE_EXECUTE_READWRITE
-                    | winnt::PAGE_EXECUTE_WRITECOPY
-                    | winnt::PAGE_READWRITE
-                    | winnt::PAGE_WRITECOPY;
-
-                let regions = process
-                    .memory_regions()
-                    .into_iter()
-                    .filter(|p| (p.Protect & MASK) != 0)
-                    .collect::<Vec<_>>();
-
-                println!("Scanning {} memory regions", regions.len());
-                let scan = scan_info.to_scan(&value_type).unwrap();
-                let last_scan = process.scan_regions(&regions, scan);
-                println!(
-                    "Found {} locations",
-                    last_scan.iter().map(|r| r.locations.len()).sum::<usize>()
-                );
-                *state.opened_process.lock().unwrap() = Some(process);
-                *state.scan_value_type.lock().unwrap() = value_type;
-                *state.last_scan.lock().unwrap() = last_scan;
+            fn main() {
+                tauri::Builder::default()
+                    .manage(AppState::new())
+                    .invoke_handler(tauri::generate_handler![
+                        get_processes,
+                        get_opened_process,
+                        $(
+                            [<write_opened_process_memory_ $type>],
+                            [<get_last_scan_ $type>],
+                            [<first_scan_ $type>],
+                            [<next_scan_ $type>],
+                        )+
+                    ])
+                    .run(tauri::generate_context!())
+                    .expect("error while running tauri application");
             }
 
-            #[tauri::command]
-            fn next_scan(scan_info: ScanInfo, state: tauri::State<AppState>) {
-                let scan = scan_info
-                    .to_scan(&state.scan_value_type.lock().unwrap())
-                    .unwrap();
-                let last_scan = state
-                    .opened_process
-                    .lock()
-                    .unwrap()
-                    .as_ref()
-                    .unwrap()
-                    .rescan_regions(&state.last_scan.lock().unwrap(), scan);
-                println!(
-                    "Now have {} locations",
-                    last_scan.iter().map(|r| r.locations.len()).sum::<usize>()
-                );
-                *state.last_scan.lock().unwrap() = last_scan;
-            }
-        })*
+            $(
+                #[tauri::command]
+                fn [<write_opened_process_memory_ $type>](
+                    address: usize,
+                    value: $type,
+                    state: tauri::State<AppState>,
+                ) -> Option<usize> {
+                    state
+                        .opened_process
+                        .lock()
+                        .unwrap()
+                        .as_ref()
+                        .unwrap()
+                        .write_memory(address, &value)
+                        .ok()
+                }
+
+                #[tauri::command]
+                fn [<get_last_scan_ $type>](state: tauri::State<AppState>) -> Vec<Region<$type_size, $type>> {
+                    state.[<last_scan_ $type>].lock().unwrap().clone()
+                }
+
+                #[tauri::command]
+                fn [<first_scan_ $type>](pid: u32, value_type: ValueType, scan_info: ScanInfo, state: tauri::State<AppState>) {
+                    let process = Process::open(pid).unwrap();
+                    println!("Opened process {:?}", process);
+
+                    const MASK: u32 = winnt::PAGE_EXECUTE_READWRITE
+                        | winnt::PAGE_EXECUTE_WRITECOPY
+                        | winnt::PAGE_READWRITE
+                        | winnt::PAGE_WRITECOPY;
+
+                    let regions = process
+                        .memory_regions()
+                        .into_iter()
+                        .filter(|p| (p.Protect & MASK) != 0)
+                        .collect::<Vec<_>>();
+
+                    println!("Scanning {} memory regions", regions.len());
+                    let scan = scan_info.to_scan(&value_type).unwrap();
+                    let last_scan = process.scan_regions(&regions, scan);
+                    println!(
+                        "Found {} locations",
+                        last_scan.iter().map(|r| r.locations.len()).sum::<usize>()
+                    );
+                    *state.opened_process.lock().unwrap() = Some(process);
+                    *state.scan_value_type.lock().unwrap() = value_type;
+                    *state.[<last_scan_ $type>].lock().unwrap() = last_scan;
+                }
+
+                #[tauri::command]
+                fn [<next_scan_ $type>](scan_info: ScanInfo, state: tauri::State<AppState>) {
+                    let scan = scan_info
+                        .to_scan(&state.scan_value_type.lock().unwrap())
+                        .unwrap();
+                    let last_scan = state
+                        .opened_process
+                        .lock()
+                        .unwrap()
+                        .as_ref()
+                        .unwrap()
+                        .rescan_regions(&state.[<last_scan_ $type>].lock().unwrap(), scan);
+                    println!(
+                        "Now have {} locations",
+                        last_scan.iter().map(|r| r.locations.len()).sum::<usize>()
+                    );
+                    *state.[<last_scan_ $type>].lock().unwrap() = last_scan;
+                }
+            )+
+        }
     }
 }
 
-impl_scan!(i32: 4);
+impl_scan!(i8: 1, u8: 1, i16: 2, u16:2 , i32: 4, u32: 4, i64: 8, u64: 8);
