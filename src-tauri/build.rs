a0b8000d07f45caf4337dfb3ab817b07b2cffa4f
Mohsen Pakzad <mohsen137853@gmail.com>
2022-10-18 21:28:39 +0330

    Add manifest to app to always run with administrator access

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,9 @@
 fn main() {
-  tauri_build::build()
+    tauri_build::build();
+    if !cfg!(debug_assertions) && cfg!(target_os = "windows") {
+        let mut res = winres::WindowsResource::new();
+        res.set_icon("icons/icon.ico");
+        res.set_manifest_file("Urule.exe.manifest");
+        res.compile().unwrap();
+    }
 }
