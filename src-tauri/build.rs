05e41892b99e85c5f99ed13cc2c68131270e6fd4
Mohsen Pakzad <mohsen137853@gmail.com>
2022-10-19 13:42:18 +0330

    Remove custom manifest file due it's override tauri app configuration and cause strange behaviors

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,9 +1,3 @@
 fn main() {
-    tauri_build::build();
-    if !cfg!(debug_assertions) && cfg!(target_os = "windows") {
-        let mut res = winres::WindowsResource::new();
-        res.set_icon("icons/icon.ico");
-        res.set_manifest_file("Urule.exe.manifest");
-        res.compile().unwrap();
-    }
+    tauri_build::build()
 }
